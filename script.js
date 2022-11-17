0b0f181ce2aa4bff9157810ad122e091f1fec4e8
RjSaguino <jrsaguino@gmail.com>
2022-11-17 21:38:49 +0800

    version 2.0

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const convert = () => {
-  let celsius = document.getElementById("celsius");
-  let fahrenheit = celsius.value * (9 / 5) + 32;
-  let resultBox = document.querySelector("#result");
-  resultBox.innerHTML =
-    celsius.value + " °C = " + fahrenheit.toFixed(2) + " °F";
-  celsius.value = null;
-};
+const celsius = document.getElementById("celsius");
+celsius.addEventListener("input", () => {
+  let x = celsius.value * (9 / 5) + 32;
+  document.getElementById("fahrenheit").value = Math.round(x * 100) / 100;
+});
+
+const fahrenheit = document.getElementById("fahrenheit");
+fahrenheit.addEventListener("input", () => {
+  let x = (fahrenheit.value - 32) * (5 / 9);
+  document.getElementById("celsius").value = Math.round(x * 100) / 100;
+});
